177f990b5e8215d7abf7dbe5e3f9adcb1b5582e2
Lubem <32154149+Lubem@users.noreply.github.com>
2017-09-29 11:15:17 +0800

    Add files via upload
    database

---
+++ ducon.sql
@@ -0,0 +1,134 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50623
+Source Host           : localhost:3306
+Source Database       : ducon
+
+Target Server Type    : MYSQL
+Target Server Version : 50623
+File Encoding         : 65001
+
+Date: 2017-09-29 11:03:46
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `info_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `discusser_id` int(11) DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for info
+-- ----------------------------
+DROP TABLE IF EXISTS `info`;
+CREATE TABLE `info` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `brief` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `read_count` int(255) DEFAULT NULL,
+  `like_count` int(255) DEFAULT NULL,
+  `ctime` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `comment_id` int(11) DEFAULT NULL,
+  `topic_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for local_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `local_auth`;
+CREATE TABLE `local_auth` (
+  `id` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKr2dk1x2kt84b3whdedlb2slxb` (`user_id`),
+  CONSTRAINT `FKr2dk1x2kt84b3whdedlb2slxb` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of local_auth
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for oauth
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth`;
+CREATE TABLE `oauth` (
+  `id` varchar(255) NOT NULL,
+  `oauth_access_token` varchar(255) DEFAULT NULL,
+  `oauth_expires` varchar(255) DEFAULT NULL,
+  `oauth_id` varchar(255) DEFAULT NULL,
+  `oauth_name` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK7a23c0j3h2lvkrqjy7hkpx4hi` (`user_id`),
+  CONSTRAINT `FK7a23c0j3h2lvkrqjy7hkpx4hi` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of oauth
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for topic
+-- ----------------------------
+DROP TABLE IF EXISTS `topic`;
+CREATE TABLE `topic` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `head_img` varchar(255) DEFAULT NULL,
+  `back_img` varchar(255) DEFAULT NULL,
+  `brief` varchar(255) DEFAULT NULL,
+  `att_count` int(11) DEFAULT NULL,
+  `info_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of topic
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `log_time` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `qq` varchar(255) DEFAULT NULL,
+  `reg_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
