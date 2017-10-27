a4eb8c6f8f3bf5cc5977cb34be434919380620c1
卢斌 <895112552@qq.com>
2017-10-27 23:05:31 +0800

    '修改注册表单认证'

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,45 +16,69 @@ class AuthController extends Controller
     //
     public function register (Request $request)
     {
-        $uname = $request->get('uname');
-        $upwd = $request->get('upwd');
-        $uconfirm_pwd = $request->get('uconfirm_pwd');
-        $umail = $request->get('umail');
+//        $uname = $request->get('uname');
+//        $upwd = $request->get('upwd');
+//        $uconfirm_pwd = $request->get('uconfirm_pwd');
+//        $umail = $request->get('umail');
+//
+//        //username is exists?
+//        $boo_name = LocalAuth::where('username', $uname)->exists();
+//
+//        if(empty($uname) || empty($upwd) || empty($umail)){
+//            return response()->json([
+//                'code' => 451,
+//            ]);
+//        }else if($upwd != $uconfirm_pwd || !\MailValid::isEmail($umail) || strlen($uname) < 6 || strlen($upwd) < 6){
+//            return response()->json([
+//                'code' => 452,
+//            ]);
+//        }else if($boo_name){
+//            return response()->json([
+//                'code' => 453,
+//            ]);
+//        }else{
+//            $userId = DB::table('user')->insertGetId([
+//                'name' => $uname,
+//                'email' => $umail,
+//                'reg_time' =>  Carbon::now()->toDateTimeString(),
+//            ]);
+//            $local_auth = LocalAuth::create([
+//                'username' => $uname,
+//                'password' => Hash::make($upwd),
+//                'email' => $umail,
+//                'phone' => '',
+//                'user_id' => $userId,
+//                //'phone', 'email', 'password', 'username', 'user_id'
+//            ]);
+//            return response()->json([
+//                'code' => 200,
+//                'url' => url('/login')
+//            ]);
+//        }
 
-        //username is exists?
-        $boo_name = LocalAuth::where('username', $uname)->exists();
+        $this->validate($request, [
+            'username' => 'required|between:4,16|unique:user,name|alpha_dash',
+            'password' => 'required|between:6,12|confirmed',
+            'email' => 'required|email|unique:user,email'
+        ]);
 
-        if(empty($uname) || empty($upwd) || empty($umail)){
-            return response()->json([
-                'code' => 451,
-            ]);
-        }else if($upwd != $uconfirm_pwd || !\MailValid::isEmail($umail) || strlen($uname) < 6 || strlen($upwd) < 6){
-            return response()->json([
-                'code' => 452,
-            ]);
-        }else if($boo_name){
-            return response()->json([
-                'code' => 453,
-            ]);
-        }else{
-            $userId = DB::table('user')->insertGetId([
-                'name' => $uname,
-                'email' => $umail,
-                'reg_time' =>  Carbon::now()->toDateTimeString(),
-            ]);
-            $local_auth = LocalAuth::create([
-                'username' => $uname,
-                'password' => Hash::make($upwd),
-                'email' => $umail,
-                'phone' => '',
-                'user_id' => $userId,
-                //'phone', 'email', 'password', 'username', 'user_id'
-            ]);
-            return response()->json([
-                'code' => 200,
-                'url' => url('/login')
-            ]);
-        }
+        $userId = DB::table('user')->insertGetId([
+            'name' => $request->get('username'),
+            'email' => $request->get('email'),
+            'reg_time' =>  Carbon::now()->toDateTimeString(),
+        ]);
+        $local_auth = LocalAuth::create([
+            'username' => $request->get('username'),
+            'password' => Hash::make($request->get('password')),
+            'email' => $request->get('email'),
+            'phone' => '',
+            'user_id' => $userId,
+            //'phone', 'email', 'password', 'username', 'user_id'
+        ]);
+        return response()->json([
+            'code' => 200,
+            'url' => url('/login')
+        ]);
     }
 
     public function login(Request $request)
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,36 +8,13 @@
 <body>
 <div id="app">
         <form @submit.prevent="submit" style="margin-top: 5%">
-            {{--<form-item required label="用户名:" label-col="4" wrapper-col="12" has-icon tips="我是提示" tips-mode="popup">--}}
-                {{--<v-input type="text" placeholder="请输入您的用户名" v-model="user.username" required required-tips="用户名为必填项"></v-input>--}}
-            {{--</form-item>--}}
-            {{--<form-item required label="密码:" label-col="4" wrapper-col="12" has-icon tips="我是提示">--}}
-                {{--<v-input type="password" placeholder="请输入您的密码" v-model="user.pwd"></v-input>--}}
-            {{--</form-item>--}}
-            {{--<form-item required label="确认密码:" label-col="4" wrapper-col="12" has-icon tips="我是提示">--}}
-                {{--<v-input type="password" placeholder="请确认您的密码" v-model="user.confirm_pwd"></v-input>--}}
-            {{--</form-item>--}}
-            {{--<form-item required label="电话:" label-col="4" wrapper-col="12" has-icon :valid-status="phoneValidResult.isPhoneValid.validStatus" :tips="phoneValidResult.isPhoneValid.tips">--}}
-                {{--<v-input type="text" placeholder="请输入您的电话号码" :rules="phoneValidRule" :valid-result="phoneValidResult" v-model="user.tel"></v-input>--}}
-            {{--</form-item>--}}
-            {{--<form-item required label="邮箱:" label-col="4" wrapper-col="12" has-icon tips="我是提示">--}}
-                {{--<v-input type="text" placeholder="请输入您的邮箱" v-model="user.mail"></v-input>--}}
-            {{--</form-item>--}}
-            {{--<form-item required label="性别:" label-col="4" wrapper-col="12">--}}
-                {{--<label><input type="radio" value="男" v-model="user.gender" checked />男</label>--}}
-                {{--<label><input type="radio" value="女" v-model="user.gender" />女</label>--}}
-            {{--</form-item>--}}
-            {{--<form-item required label="个性签名" label-col="4" wrapper-col="12" tips-mode="popup" description-width="500" description-space="500">--}}
-                {{--<v-input type="text" v-model="user.sign"></v-input>--}}
-            {{--</form-item>--}}
-            {{--<form-item label-col="4" wrapper-col="12">--}}
-                {{--<v-button @click.native="submit" primary style="margin-right:10px">确定</v-button><v-button type="reset" tertiary value="重置条件"></v-button>--}}
-            {{--</form-item>--}}
-
-            <at-input v-model="user.uname" placeholder="用户名" :status="status.name"></at-input>
-            <at-input v-model="user.upwd" type="password" placeholder="密码" :status="status.pwd"></at-input>
-            <at-input v-model="user.uconfirm_pwd" type="password" placeholder="确认密码" :status="status.confirm_pwd"></at-input>
-            <at-input v-model="user.umail" placeholder="邮箱" :status="status.mail"></at-input>
+            <at-input v-model="user.username" placeholder="用户名" :status="status.username"></at-input>
+            <div v-if="errors.hasOwnProperty('username')">@{{ errors.username }}</div>
+            <at-input v-model="user.password" type="password" placeholder="密码" :status="status.password"></at-input>
+            <at-input v-model="user.password_confirmation" type="password" placeholder="确认密码" :status="status.password_confirmation"></at-input>
+            <div v-if="errors.hasOwnProperty('password')">@{{ errors.password }}</div>
+            <at-input v-model="user.email" placeholder="邮箱" :status="status.email"></at-input>
+            <div v-if="errors.hasOwnProperty('email')">@{{ errors.email }}</div>
             <at-button type="info" hollow>注册</at-button>
         </form>
 </div>
@@ -51,92 +28,91 @@
         el:'#app',
         data: {
             user: {
-                uname: '',
-                upwd: '',
-                uconfirm_pwd: '',
-                umail: '',
+                username: '',
+                password: '',
+                password_confirmation: '',
+                email: '',
             },
             status: {
-                name: '',
-                pwd: '',
-                confirm_pwd: '',
-                mail: '',
+                username: '',
+                password: '',
+                password_confirmation: '',
+                email: '',
             },
+            errors: [],
         },
         components: {
 
         },
         methods: {
             submit: function () {
-                //var formData = JSON.stringify(this.user);
-                let { name, pwd, confirm_pwd, mail } = this.status;
-                let { uname, upwd, uconfirm_pwd, umail } = this.user;
-                if(name == 'error' || pwd == 'error' || confirm_pwd == 'error' || mail == 'error'){
-                    alert('输入有误，请检查后重新输入')
-                    return;
-                }else if(uname == "" || upwd == "" || uconfirm_pwd == "" || umail == ""){
-                    alert('输入不能为空')
-                }
-                else{
+                var these = this
                     axios({
                         method: 'post',
                         url: '/user/register',
                         data: this.user,
                     })
-                        .then(function (response) {
-                            if (response.data.code == 200) {
-                                alert('成功！')
-                                window.location.href = response.data.url
-                            }else if (response.data.code == 451) {
-                                alert('服务器内部错误，输入不能为空！')
-                            }else if (response.data.code == 452) {
-                                alert('服务器内部错误，验证错误！')
-                            }else if (response.data.code == 453) {
-                                alert('用户名已存在，请重新输入！')
-                            }
-                            console.log('error --- ' + response);
-                        })
-                        .catch(function (error) {
-                            console.log(error);
-                        });
-                }
+                    .then(function (response) {
+                        if (response.data.code == 200) {
+                            alert('注册成功！')
+                            window.location.href = response.data.url
+                        }
+                    })
+                    .catch(function (error) {
+                        console.log(error.response.data.errors)
+                        these.errors = error.response.data.errors
+                        console.log(these.errors)
+                    });
+                },
             },
-        },
         watch: {
-            'user.uname': function (val, oldval){
+            'user.username': function (val, oldval){
                 if(/^[a-zA-Z0-9_-]{4,16}$/.test(val)){
-                    this.status.name = 'success'
+                    if(this.errors.hasOwnProperty('username')){
+                        delete this.errors.username
+                    }
+                    this.status.username = 'success'
                 }else{
-                    this.status.name = 'error'
+                    this.status.username = 'error'
                 }
             },
-            'user.upwd': function (val, oldval){
-                //
-                if(Object.is(val, this.user.uconfirm_pwd)){
-                    this.status.confirm_pwd = 'success'
-                }else if(this.user.uconfirm_pwd.length > 0){
-                    this.status.confirm_pwd = 'error'
+            'user.password': function (val, oldval){
+                if(this.errors.hasOwnProperty('password')){
+                    delete this.errors.password
                 }
                 //
-                if(val.length >= 6){
-                    this.status.pwd = 'success'
+                if(Object.is(val, this.user.password_confirmation) && val.length >= 6){
+                    this.status.password_confirmation = 'success'
+                }else if(this.user.password_confirmation.length > 0){
+                    this.status.password_confirmation = 'error'
+                }
+                //
+                if(val.length >= 6 && val.length <= 12){
+                    this.status.password = 'success'
                 }
                 else{
-                    this.status.pwd = 'error'
+                    this.status.password = 'error'
                 }
             },
-            'user.uconfirm_pwd': function (val, oldval){
-                if(Object.is(val, this.user.upwd) && val.length >= 6){
-                    this.status.confirm_pwd = 'success'
+            'user.password_confirmation': function (val, oldval){
+                if(this.errors.hasOwnProperty('password')){
+                    delete this.errors.password
+                }
+                //
+                if(Object.is(val, this.user.password) && val.length >= 6){
+                    this.status.password_confirmation = 'success'
                 }else{
-                    this.status.confirm_pwd = 'error'
+                    this.status.password_confirmation = 'error'
                 }
             },
-            'user.umail': function (val, oldval) {
+            'user.email': function (val, oldval) {
                 if(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(val)){
-                    this.status.mail = 'success'
+                    if(this.errors.hasOwnProperty('email')){
+                        delete this.errors.email
+                    }
+                    this.status.email = 'success'
                 }else{
-                    this.status.mail = 'error'
+                    this.status.email = 'error'
                 }
             }
         }
